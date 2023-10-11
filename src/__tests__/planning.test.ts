abbe1dfd2356bdae5cbba68b72c3d2c38f832d5c
Jonathan Dahan <git@jonathan.is>
2023-10-11 17:19:33 -0400

    Fix planning tests

--- src/__tests__/planning.test.ts
+++ src/__tests__/planning.test.ts
@@ -1,73 +1,84 @@
-import {Plan, plan, Device, AxidrawFast, XYMotion, PenMotion, defaultPlanOptions} from '../planning';
-import {Vec2} from '../vec';
+import {Plan, plan, profiles, PenMotion} from '../planning'
+import { Vec2 } from '../vec'
 
 describe("plan", () => {
-  const device = Device()
-  const positions = {
-    up: AxidrawFast.penUpPos,
-    down: AxidrawFast.penDownPos,
-    zero: device.penPctToPos(0)
-  }
+  const profile = profiles.v3
+  const up = profile.penUpPos
+  const down = profile.penDownPos
+  const origin = { x: 0, y: 0 }
+  
+  // We always start and end in the up position
   it.skip("handles an empty input", () => {
-    expect(plan([], AxidrawFast)).toEqual(new Plan([], positions.zero))
+    expect(plan([], profile)).toEqual(new Plan([]))
   });
 
-  function xyMotions(plan: Plan) {
-    let curPenPos = 0;
-    const motions: {from: Vec2; to: Vec2; penPos: number}[] = [];
-    for (const m of plan.motions) {
-      if (m instanceof PenMotion) {
-        curPenPos = m.finalPos;
-      } else if (m instanceof XYMotion) {
-        motions.push({from: m.p1, to: m.p2, penPos: curPenPos});
-      }
-    }
-    return motions;
-  }
+  // This turns a full Plan into simplified json for easier testing
+  function motions(plan: Plan): Array<{pen: number}|{from: Vec2, to: Vec2}> {
+    return plan
+      .motions
+      .map(motion => {
+        if (motion instanceof PenMotion) return {pen: motion.finalPos}
+        return {from: motion.p1, to: motion.p2}
+      })
+}
 
   it("handles a single point input", () => {
-    const p = plan([[{x: 10, y: 10}]], AxidrawFast);
-
-    expect(xyMotions(p)).toEqual([
-      {from: {x: 0, y: 0}, to: {x: 10, y: 10}, penPos: 0},
-      {from: {x: 10, y: 10}, to: {x: 10, y: 10}, penPos: positions.up},
-      {from: {x: 10, y: 10}, to: {x: 0, y: 0}, penPos: positions.zero},
-    ]);
-  });
+    const point = { x: 10, y: 10 }
+    
+    const singlePoint = plan([[point]], profile)
+    
+    expect(motions(singlePoint)).toEqual([
+      {pen: up},
+      {from: origin, to: point},
+      {pen: down},
+      {from: point, to: point},
+      {pen: up},
+      {from: point, to: origin},
+    ])
+  })
 
   it("handles a line", () => {
-    const p = plan([[{x: 10, y: 10}, {x: 20, y: 10}]], AxidrawFast);
+    const line = [{x: 10, y: 10}, {x: 20, y: 10}]
+    const original = plan([line], profile);
 
-    expect(xyMotions(p)).toEqual([
-      {from: {x: 0, y: 0}, to: {x: 10, y: 10}, penPos: 0},
-      {from: {x: 10, y: 10}, to: {x: 20, y: 10}, penPos: positions.up},
-      {from: {x: 20, y: 10}, to: {x: 0, y: 0}, penPos: positions.zero},
-    ]);
-  });
+    expect(motions(original)).toEqual([
+      {pen: up},
+      {from: origin, to: line[0]},
+      {pen: down},
+      {from: line[0], to: line[1]},
+      {pen: up},
+      {from: line[1], to: origin},
+    ])
+  })
 
   it("handles two lines", () => {
-    const p = plan([
-      [{x: 10, y: 10}, {x: 20, y: 10}],
-      [{x: 10, y: 20}, {x: 20, y: 20}],
-    ], AxidrawFast);
+    const horizontal = [{x: 10, y: 10}, {x: 20, y: 10}]
+    const vertical = [{x: 20, y: 10}, {x: 20, y: 20}]
 
-    expect(xyMotions(p)).toEqual([
-      {from: {x: 0, y: 0}, to: {x: 10, y: 10}, penPos: 0},
-      {from: {x: 10, y: 10}, to: {x: 20, y: 10}, penPos: positions.down},
-      {from: {x: 20, y: 10}, to: {x: 10, y: 20}, penPos: positions.up},
-      {from: {x: 10, y: 20}, to: {x: 20, y: 20}, penPos: positions.down},
-      {from: {x: 20, y: 20}, to: {x: 0, y: 0}, penPos: positions.zero},
-    ]);
-  });
+    const twoLines = plan([horizontal, vertical], profile)
+    console.log(motions(twoLines))
+    expect(motions(twoLines)).toEqual([
+      {pen: up},
+      {from: origin, to: horizontal[0]},
+      {pen: down},
+      {from: horizontal[0], to: horizontal[1]},
+      {pen: up}, // Will this be removed with joinLines Optimization?
+      {from: horizontal[1], to: vertical[0]},
+      {pen: down},
+      {from: vertical[0], to: vertical[1]},
+      {pen: up},
+      {from: vertical[1], to: origin},
+    ])
+  })
 
   it("shouldn't slow down for a fake point", () => {
     const p1 = plan([
       [{x: 10, y: 10}, {x: 30, y: 10}],
-    ], AxidrawFast);
+    ], profile)
     const p2 = plan([
       [{x: 10, y: 10}, {x: 25, y: 10}, {x: 30, y: 10}],
-    ], AxidrawFast);
+    ], profile)
 
-    expect(p1.motions[2].duration()).toEqual(p2.motions[2].duration());
+    expect(p1.motions[2].duration()).toEqual(p2.motions[2].duration())
   })
-});
+})
