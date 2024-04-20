9f14d5edda32b0d11033ed849e3fb6b2c1628738
Alex Ruddick <alex@ruddick.tech>
2024-04-20 11:46:59 -0500

    migrate to v4

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
-import "web-streams-polyfill/es2018"
+import "web-streams-polyfill/polyfill"
 import express from "express";
 import http from "http";
 import path from "path";
