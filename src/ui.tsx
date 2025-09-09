1430ace325e77e868f85c0eb0f9bda7b0eba9262
Alex Ruddick <alex@ruddick.tech>
2025-09-08 23:22:03 -0400

    Add NextDraw 2234

--- src/ui.tsx
+++ src/ui.tsx
@@ -262,6 +262,7 @@ function HardwareOptions({ state, driver }: { state: State, driver: BaseDriver |
       >
         <option value="v3">AxiDraw V3</option>
         <option value="brushless">AxiDraw V3 Brushless</option>
+        <option value="nextdraw-2234">NextDraw 2234</option>
       </select>
     </label>
   </div>;
