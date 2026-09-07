72d6cde5c3883f435a68a05e5aa98ee656749dd1
Alex Ruddick <alex@ruddick.tech>
2026-09-07 15:25:18 -0500

    change penServoMin and penServoMax for NextDraw2234

--- src/planning.ts
+++ src/planning.ts
@@ -141,8 +141,8 @@ const AxidrawBrushless: Device = {
 const NextDraw2234: Device = {
   stepsPerMm: 5,
 
-  penServoMin: 19600, // pen down - 70% of range
-  penServoMax: 28000, // pen up - full range
+  penServoMin: 5400, // pen down
+  penServoMax: 12600, // pen up
 
   penPctToPos(pct: number): number {
     const t = pct / 100.0;
